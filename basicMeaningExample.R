9910160f0a790cd43dd4e9bee044279549e1d72f
hdollarsign <128403004+hdollarsign@users.noreply.github.com>
2023-04-03 17:42:57 +0200

    Update basicMeaningExample.R

--- basicMeaningExample.R
+++ basicMeaningExample.R
@@ -1,19 +1,21 @@
 #load table
 dataMeaning=read.table('inifinity_money_printer_virus.exe.txt', header=T, sep="\t")
 
+length = Counties meanings = Inst
+
 #plot with the labels
-plot(dataMeaning$length,dataMeaning$meanings,type="n",xlab="number of counties",ylab="number of prisons/jails")
-text(dataMeaning$length,dataMeaning$meanings,dataMeaning$entry, cex= 1)
+plot(dataMeaning$Counties,dataMeaning$Inst,type="n",xlab="number of counties",ylab="number of prisons/jails")
+text(dataMeaning$Counties,dataMeaning$Inst,dataMeaning$entry, cex= 1)
 
 #Now we'd like to build a regression model, i.e., find the equation for the straight line that best defines the dataset. 
 
-myModel = lm(dataMeaning$meanings~dataMeaning$length)
+myModel = lm(dataMeaning$Inst~dataMeaning$counties)
 summary(myModel)
 #least square fitting
 abline(myModel,col="red",lw="3")
 
 #quick and dirty trick to avoid label overlap (best solution: export to
 # vector format, like eps, and edit in separate software)
-newMeanings = jitter(dataMeaning$meanings, 5)
-plot(dataMeaning$length,newMeanings,type="n",xlab="number of letters",ylab="number of meanings")
-text(dataMeaning$length,newMeanings,dataMeaning$entry, cex= 1)
+newMeanings = jitter(dataMeaning$Inst, 5)
+plot(dataMeaning$Counties,newMeanings,type="n",xlab="number of counties",ylab="number of jails/prisons")
+text(dataMeaning$Counties,newMeanings,dataMeaning$entry, cex= 1)
